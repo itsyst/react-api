704108611456971be141bb0c33a9a967178889e6
Khaled El Hamzi <contact@elhamzi.me>
2021-11-02 21:31:33 +0100

    Optimistic  updates.

--- src/App.js
+++ src/App.js
@@ -32,11 +32,19 @@ class App extends Component {
     this.setState({ posts })
   };
 
-  handleDelete = async(post) => {
-    await axios.delete(apiEndpoint + '/' + post.id);
-
+  handleDelete = async (post) => {
+    const originalPosts = this.state.posts;
     const posts = this.state.posts.filter(p => p.id !== post.id);
     this.setState({ posts })
+
+    try {
+      await axios.delete(apiEndpoint + '/' + post.id);
+      throw new Error('');
+    }
+    catch (error) {
+      alert('Something failed while deleting a post!');
+      this.setState({ posts: originalPosts });
+    }
   };
 
   render() {
