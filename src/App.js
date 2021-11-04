124d0fa04f498d7819ee4cbf828d76e105a9bf9c
Khaled El Hamzi <contact@elhamzi.me>
2021-11-04 12:15:00 +0100

    Install toastify package.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import http from './services/httpService'
 import config from './config.json';
 
